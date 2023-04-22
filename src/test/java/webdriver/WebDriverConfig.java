1ca594186475b9efbd372931cea1c5b7b28cad6f
patrickduong <duong.it1987@gmail.com>
2023-04-22 16:52:33 +0700

    add driver to win os, handle clean up cookies, add version gradle

--- src/test/java/webdriver/WebDriverConfig.java
+++ src/test/java/webdriver/WebDriverConfig.java
@@ -27,8 +27,9 @@ public class WebDriverConfig {
         } else initBrowserDriverOnMac(browserType);
 
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(SLOW_TIMEOUT));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(VERY_SLOW_TIMEOUT));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(MEDIUM_TIMEOUT));
     }
 
     public WebDriverConfig() {
